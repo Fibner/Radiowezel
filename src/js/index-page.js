3accbb703f9f5aaced0bf0801c1ccad2fb907a30
Marcin Rębilas <mrebilas7102003@gmail.com>
2022-03-31 21:24:25 +0200

    hello

--- src/js/index-page.js
+++ src/js/index-page.js
@@ -1,5 +1,6 @@
 window.onload = function(){
     document.querySelector("#logout-button").addEventListener("click", logOut);
+    document.querySelector("#musicPlayer").addEventListener("click", musicPlayer);
 }
 
 function logOut(){
@@ -11,5 +12,13 @@ function logOut(){
         }
     }).fail(function(){
         alert("Błąd serwera");
+    })
+}
+
+function musicPlayer(){
+    $.ajax({
+        success: function(){
+            location.href = "pages/musicPlayer.php"
+        }
     })
 }
