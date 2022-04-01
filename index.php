cf032a7ba5bfa61c052ea8f31267b27d7f9b78ac
aztecq <azteccx99@gmail.com>
2022-04-01 17:04:54 +0200

    Dodanie stylu do addsong

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
 session_start();
-if(!isset($_SESSION['admin']) || !$_SESSION['admin']){
+if (!isset($_SESSION['admin']) || !$_SESSION['admin']) {
     header("Location: pages/login");
 }
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -13,44 +14,33 @@ if(!isset($_SESSION['admin']) || !$_SESSION['admin']){
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="src/css/index-page.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
     <title>Document</title>
 </head>
+
 <body>
     <div id="menu-section">
         <div id="playlist-div" class="menu-button-div">
-            <button id="playlist-button">Playlista</button>
+            <input type="button" class="Pbutton" name="button" value="Playlista" />
         </div>
         <div id="logout-div" class="menu-button-div">
-            <button id="logout-button">Wyloguj</button>
+            <input type="button" class="Wbutton" name="button" value="Wyloguj" />
         </div>
     </div>
-    <table id="musicList">
-        <tr>
-            <th>URL</th>
-            <th>Likes</th>
-            <th>Dislikes</th>
-            <th>OrPlay</th>
-        </tr>
 
-        <?php
-             require "php/dbconnection.php";
+    
+    <div id="link">
+        <br><br>
+        <h1>Dodaj piosenke do playlisty</h1>
+        <hr>
+        <input type="text" name="link" id="link"  class="lin">
+        <input type="button" class="Lbutton" name="button" value="Dodaj" />
+    </div>
 
-             $sql = "SELECT * FROM music;";
-             $sql_data = $db -> query($sql);
-
-             foreach($sql_data as $data){
-                 echo("<tr>");
-                    echo("<td>".$data['url']."</td>");
-                    echo("<td>".$data['likes']."</td>");
-                    echo("<td>".$data['dislikes']."</td>");
-                    echo("<td>".$data['orPlay']."</td>");
-                 echo("</tr>");
-             }
-
-             $db ->close();
-        ?>
-    </table>
     <script src="src/js/jquery-3.6.0.js"></script>
     <script src="src/js/index-page.js"></script>
 </body>
+
 </html>
--- pages/addsong.php
+++ pages/addsong.php
@@ -12,14 +12,32 @@ if(!isset($_SESSION['admin']) || !$_SESSION['admin']){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/9ec0aafe67.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../src/css/addsong-page.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
     <title>Dodaj piosenkę do bazy</title>
 </head>
 <body>
-    <div id="add-form">
-        <label for="link">Link do youtube:</label><br>
-        <input type="text" name="link" id="link-input"><br>
-        <button id="submit-button">Dodaj</button>
+    <div id="menu-section">
+        <div id="playlist-div" class="menu-button-div">
+            <input type="button" class="Pbutton" name="button" value="Playlista" />
+        </div>
+        <div id="logout-div" class="menu-button-div">
+            <input type="button" class="Wbutton" name="button" value="Wyloguj" />
+        </div>
     </div>
+
+    
+    <div id="link-div">
+        <br><br>
+        <h1>Dodaj piosenke do playlisty</h1>
+        <hr>
+        <input type="text" name="link" id="link-input"  class="lin">
+        <input type="button" class="Lbutton" name="button" id="add-button" value="Dodaj" />
+    </div>
+
     <script src="../src/js/jquery-3.6.0.js"></script>
     <script type="module" src="../src/js/addsong-page.js"></script>
 </body>
